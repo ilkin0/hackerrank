41a4be003ef4b14ff6e904c0d42f8f9516c9f24b
Ilkin Mehdiyev <ilkin.mehdiyev@cybernet.az>
2021-01-17 01:41:36 +0400

    Day 5: Loops

---
+++ 30 Days of Code/Java/Loops.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Loops {
+
+    public static void multiplierTable(int n) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n + " x " + i + " = " + (n * i));
+        }
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        
+        multiplierTable(n);
+
+        scanner.close();
+    }
+}
